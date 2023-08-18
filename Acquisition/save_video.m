c6bc009bd664fc393e99238398ba14a60c7df89c
polbech <65780058+polbech@users.noreply.github.com>
2023-08-18 15:47:01 +0200

    Update save_video.m
    Cleaned up commented code, formatted trial numeration

--- Acquisition/save_video.m
+++ Acquisition/save_video.m
@@ -1,18 +1,3 @@
-% function save_video(src, obj, loc, q)
-%     global count
-%     count = count+1;
-%     frames = src.FramesAcquired- src.FramesAcquiredFcnCount;
-% 
-%     [data, timestamps] = getdata(src, src.FramesAvailable);
-%     
-% %     send(q, [loc '_data' num2str(count)])
-%     if mod(count, 10)==0
-%         send(q, data);
-%     end
-%     save([loc '_data' num2str(count) '.tiff' ], 'data', '-v7.3')   
-%     save([loc '_timestamps' num2str(count) '.mat' ], 'timestamps', '-v7.3')
-%     
-% end
 function save_video(src, obj, config, q)
     global count
     
@@ -24,17 +9,12 @@ function save_video(src, obj, config, q)
     end
     
     count = count+1;
-    
 
     [data, timestamps] = getdata(src, src.FramesAvailable);
     
-%     if mod(count, 1000)==0
-% %         send(q, data);
-%         send(q, count);
-%     end
     send(q, count);
 
-    save([SessionFolder TrialPrefix '_' num2str(count) '.mat' ], 'data', '-v7.3')   
-    save([SessionFolder TrialPrefix '_' num2str(count) '_timestamps.mat' ], 'timestamps', '-v7.3')
+    save([SessionFolder TrialPrefix '_' num2str(count, '%04d') '.mat' ], 'data', '-v7.3')   
+    save([SessionFolder TrialPrefix '_' num2str(count, '%04d') '_timestamps.mat' ], 'timestamps', '-v7.3')
     
 end
